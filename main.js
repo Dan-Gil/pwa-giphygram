0d7039c9a0903bc859d03a0f9a735fe25bb87193
Dan Gil <maria.jimenezglobant.com>
2022-09-02 16:15:26 -0500

    Lighthouse audit

--- main.js
+++ main.js
@@ -46,7 +46,7 @@ function update() {
 				// Add Giphy HTML
 				$("#giphys").prepend(
 					'<div class="col-sm-6 col-md-4 col-lg-3 p-1">' +
-						'<img class="w-100 img-fluid" src="' +
+						'<img class="w-100 img-fluid" alt="giphy fun" src="' +
 						giphy.images.downsized_large.url +
 						'">' +
 						"</div>"
